4a69ec53b6193b4811cfeddf06e8c6b96a00b56c
antunigr <antuniribeiro@gmail.com>
2023-06-25 22:47:51 -0300

    arrumei o cardapio

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import Interface.Pedido;
 import Interface.Pizzas;
 import model.Pizzas_Salgadas;
+import ui.CardapioGUI;
+import ui.Home;
+import ui.Pedidos;
 import util.ClassePizza;
 import util.TamanhoPizza;
 
@@ -8,13 +11,22 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Pizzas> cardapio = Pizzas_Salgadas.PizzasFactory.criarCardapio();
-        for (Pizzas pizza : cardapio) {
-            System.out.println(pizza);
-        }
+//        List<Pizzas> cardapio = Pizzas_Salgadas.PizzasFactory.criarCardapio();
+//        for (Pizzas pizza : cardapio) {
+//            System.out.println(pizza);
+//        }
+//
+//        Pedido pedido = new Pedido(new Pizzas_Salgadas.Quatro_Queijos(), TamanhoPizza.Big);
+//
+//        System.out.println(pedido.getValor());
+//
+//        Home homeUi = new Home();
+//        homeUi.homeUiSetup();
 
-        Pedido pedido = new Pedido(new Pizzas_Salgadas.Quatro_Queijos(), TamanhoPizza.Big);
+//        CardapioGUI cardapioGUI = new CardapioGUI();
+//        cardapioGUI.setVisible(true);
 
-        System.out.println(pedido.getValor());
+        Pedidos pedidos = new Pedidos();
+        pedidos.setVisible(true);
     }
 }
